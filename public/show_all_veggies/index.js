73fa809e9e8097417ffa3cba2eb7bb19d23626ff
1Porscha <117795389+1Porscha@users.noreply.github.com>
2023-03-01 00:19:37 -0500

    update

--- public/show_all_veggies/index.js
+++ public/show_all_veggies/index.js
@@ -1,47 +1,38 @@
 console.log("display page")
 // front end
 
-//sending a get request to this route
-//we are waiting for that data and we are goint to log that data
-//you should get an array of obects in the front end console
-// const getVeggie = async () => { //get data function
+// sending a get request to this route
+// we are waiting for that data and we are goint to log that data
+// you should get an array of obects in the front end console
 
-//front end
-
-let containerElement = document.getElementById('container')
+let container = document.getElementById('container')
 
 //sending a get request to this route
 //we are waiting for that data and we are goint to log that data
 //you should get an array of obects in the front end console
-const getData = async () => { //get data function
-    let data = await fetch("http://localhost:5000/veggies"); //wait for data
-    data.json().then((parsedData) => { //parse data
-        console.log(parsedData) // data is array of object    
+const getData = async () =>{ //get data function
+    let data = await fetch('/veggies'); //wait for data
+    data.json().then((parsedData) =>{ //parse data
+        console.log(parsedData); // data is array of object
         // map through and put in HTML
-        // push each individual one,  or push an array of HTML 
-        parsedData.forEach((object) => { //for each item we creat a ptag
-            // if not ready to eat- red text
-            let pTag = document.createElement("p"); // <p></p>
-            pTag.textContent = object.name; // <p>apple</p>set name, we set the text content to the name of that object
-            containerElement.appendChild(pTag) //add to container element, we add it to the end of our child list
+        // push each individual one,  or push an array of HTML
+        parsedData.forEach(object => { //for each item we creat a ptag
+            let pTag = document.createElement('p'); // <p></p>
+            pTag.textContent = object.name // <p>apple</p>set name, we set the text content to the name of that object
+            container.appendChild(pTag) //add to container element, we add it to the end of our child list
+            pTag.id = object._id
+            if (object.readyToEat){
+                pTag.style.color = "green"
+            }else{
+                pTag.style.color = "red"
+            }
 
-            // pTag.id = object._id
-            // if (element.readyToEat){
-            //     pTag.style.backgroundColor = "green"
-            // }else{
-            //     pTag.style.backgroundColor = "red"
-            // }
-            pTag.addEventListener('click', () =>{
-                console.log("click", object._id)
-                window.location.href=`../single_veggie/?Id=${object._id}`
-
+            pTag.addEventListener('click', (event) =>{
+                console.log(event.target);
+                console.log(event.target.id);
+                window.location.href = `../single_veggie?veggieId=${event.target.id}`
             })
-            
-            containerElement.appendChild(pTag)
-        })    
-    })
+        });
+    })    
 }
-
-
-getData()
-
+getData()    
--- public/single_veggie/index.js
+++ public/single_veggie/index.js
@@ -5,34 +5,25 @@ console.log("js running")
 const params = new Proxy(new URLSearchParams(window.location.search), {
     get: (searchParams, prop) => searchParams.get(prop),
 })
- let id = params.id
- 
+ let id = params.veggieId
+
 console.log(id);
 // use id to get info from collection
-
 
 //use id to get info from mongobd
 //with data from mongobd display product on page
 //create funtion and await response
-const getSingleVeggie = async () =>{
+const displayVeggie = async () =>{
     let response = await fetch(`http://localhost:5000/veggie/${id}`)
-    console.log(response)
 
     let finalResponse = await response.json()
 
     console.log(finalResponse);
 
- //use this finaResponse to display item information    
-   
-    let containerElement = document.getElementById('container')
-    containerElement.innerHTML = `
-    <div class="single-veggie">
-        <h1>${finalResponse.name}</h1>
-        <h3>${finalResponse.color}</h3>
-        <h3> Price $${finalResponse.age}</h3>
-     </div>
-    `
+//use this finaResponse to display item information 
+
+    let veggieName = document.getElementById('veggie-name')
+    veggieName.textContent = finalResponse.name    
+
 }
-getSingleVeggie()
-
-
+displayVeggie()
